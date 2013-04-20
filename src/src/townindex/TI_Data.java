a37e9a037548e1768ead8c9c2809360af4a74c31
Chris Sewell <kcbsewell2@hotmail.com>
2013-04-20 01:24:28 -0700

    More Data, More Help

--- src/src/townindex/TI_Data.java
+++ src/src/townindex/TI_Data.java
@@ -2,13 +2,14 @@ package src.townindex;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.configuration.serialization.SerializableAs;
 import org.bukkit.util.Vector;
 
 @SerializableAs("TI_Data")
-public class TI_Data implements ConfigurationSerializable{
-	
+public class TI_Data implements ConfigurationSerializable {
+
 	private String tname;
 	private String tstatus;
 	private String mayor;
@@ -16,81 +17,100 @@ public class TI_Data implements ConfigurationSerializable{
 	private String wing;
 	private Vector location;
 	private String pavail;
-	
+	private String motd;
+	private double rating;
+
 	public TI_Data(String tname) {
 		this.tname = tname;
 	}
-	
+
 	public String getTName() {
 		return tname;
 	}
-	
+
 	public void setTStatus(String tstatus) {
 		this.tstatus = tstatus;
 	}
-	
+
 	public String getTStatus() {
 		return tstatus;
 	}
-	
+
 	public void setName(String tname) {
 		this.tname = tname;
 	}
-	
+
 	public String getMayor() {
 		return mayor;
 	}
-	
+
 	public void setMayor(String mayor) {
 		this.mayor = mayor;
 	}
-	
+
 	public String getVMayor() {
 		return vmayor;
 	}
-	
+
 	public void setVMayor(String vmayor) {
 		this.vmayor = vmayor;
 	}
-	
-	
+
 	public String getWing() {
 		return wing;
 	}
-	
+
 	public void setWing(String wing) {
 		this.wing = wing;
 	}
-	
+
 	public Vector getLocation() {
 		return location;
 	}
-	
+
 	public void setLocation(Vector location) {
 		this.location = location;
 	}
-	
+
 	public String getPAvail() {
 		return pavail;
 	}
-	
+
 	public void setPAvail(String pavail) {
 		this.pavail = pavail;
 	}
-	
+
+	public String getMOTD() {
+		return motd;
+	}
+
+	public void setMOTD(String motd) {
+		this.motd = motd;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
 	@Override
 	public Map<String, Object> serialize() {
-		 Map<String, Object> tmap = new LinkedHashMap<String, Object>();
+		Map<String, Object> tmap = new LinkedHashMap<String, Object>();
 
-		 tmap.put("tname", tname);
-		 tmap.put("tstatus", tstatus);
-		 tmap.put("mayor", mayor);
-		 tmap.put("vmayor", vmayor);
-		 tmap.put("wing", wing);
-		 tmap.put("location", location);
-		 tmap.put("pavail", pavail);
+		tmap.put("tname", tname);
+		tmap.put("tstatus", tstatus);
+		tmap.put("mayor", mayor);
+		tmap.put("vmayor", vmayor);
+		tmap.put("wing", wing);
+		tmap.put("location", location);
+		tmap.put("pavail", pavail);
+		tmap.put("motd", motd);
+		tmap.put("rating", rating);
 
-		 return tmap;
+		return tmap;
 	}
 
 	public static TI_Data deserialize(Map<String, Object> tmap) {
@@ -100,17 +120,30 @@ public class TI_Data implements ConfigurationSerializable{
 		String wing = "";
 		Vector location = new Vector();
 		String pavail = "";
+		String motd = "";
+		double rating = 0;
 
-		if (!tmap.containsKey("tname")) return null;
+		if (!tmap.containsKey("tname"))
+			return null;
 		String tname = tmap.get("tname").toString();
-		
-		if (tmap.get("tstatus") != null) tstatus = tmap.get("tstatus").toString();
-		if (tmap.get("mayor") != null) mayor = tmap.get("mayor").toString();
-		if (tmap.get("vmayor") != null) vmayor = tmap.get("vmayor").toString();
-		if (tmap.get("wing") != null) wing = tmap.get("wing").toString();
-		if (tmap.get("location") != null) location = (Vector) tmap.get("location");
-		if (tmap.get("pavail") != null) pavail = tmap.get("pavail").toString();
-		
+
+		if (tmap.get("tstatus") != null)
+			tstatus = tmap.get("tstatus").toString();
+		if (tmap.get("mayor") != null)
+			mayor = tmap.get("mayor").toString();
+		if (tmap.get("vmayor") != null)
+			vmayor = tmap.get("vmayor").toString();
+		if (tmap.get("wing") != null)
+			wing = tmap.get("wing").toString();
+		if (tmap.get("location") != null)
+			location = (Vector) tmap.get("location");
+		if (tmap.get("pavail") != null)
+			pavail = tmap.get("pavail").toString();
+		if (tmap.get("motd") != null)
+			motd = tmap.get("motd").toString();
+		if (tmap.get("rating") != null)
+			rating = (double) tmap.get("rating");
+
 		TI_Data tmapfinish = new TI_Data(tname);
 		tmapfinish.tname = tname;
 		tmapfinish.tstatus = tstatus;
@@ -119,6 +152,8 @@ public class TI_Data implements ConfigurationSerializable{
 		tmapfinish.wing = wing;
 		tmapfinish.location = location;
 		tmapfinish.pavail = pavail;
+		tmapfinish.motd = motd;
+		tmapfinish.rating = rating;
 		return tmapfinish;
 	}
 }
--- src/src/townindex/TI_Main.java
+++ src/src/townindex/TI_Main.java
@@ -1,23 +1,23 @@
 package src.townindex;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.ChatColor;
-import java.util.Map;
-import org.bukkit.util.Vector;
 import org.bukkit.entity.Player;
-
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public class TI_Main extends JavaPlugin {
 	Map<String, TI_Data> tlist = new HashMap<String, TI_Data>();
@@ -27,19 +27,21 @@ public class TI_Main extends JavaPlugin {
 	private File townsFile = null;
 	private FileConfiguration tlistf = null;
 	private File tlistFile = null;
+
 	@Override
-	public void onEnable(){
+	public void onEnable() {
 		System.out.println("Town Index Enabled!");
 		File file = new File(getDataFolder() + File.separator + "config.yml");
 
-	    if (!file.exists()) {
+		if (!file.exists()) {
 			getLogger().info("Generating config.yml...");
-	      
+
 			getConfig().addDefault("townadd", "&2Town added!");
 			getConfig().addDefault("townpresent", "&4Town already exists.");
 			getConfig().addDefault("tiremove", "&2Town removed!");
 			getConfig().addDefault("tina", "&4Town does not exist.");
-			getConfig().addDefault("tihelp", "&6Use command as /ti list, /ti info <town>, or /ti edit <town>.");
+			getConfig().addDefault("tihelp", "&6Use command as /ti <info|list|edit>.");
+			getConfig().addDefault("tiadminhelp", "&6Admins use command as /ti <add|remove>");
 			getConfig().addDefault("listname", "&6Towns of Cubeville!");
 			getConfig().addDefault("tipermission", "&4You do not have permission to use this command.");
 			getConfig().addDefault("tilistsize", "&4There are not that many towns on Cubeville!");
@@ -48,24 +50,28 @@ public class TI_Main extends JavaPlugin {
 			getConfig().addDefault("wing3", "iron");
 			getConfig().addDefault("wing4", "diamond");
 			getConfig().addDefault("tiedit", "&aEdit to the town has been made successfully!");
-	      
+			getConfig().addDefault("tiedithelpbasic", "&6Use as /ti edit <town> <plots|location|wing|vmayor>.");
+			getConfig().addDefault("tiedithelpadmin", "&6Admins use /ti edit <mayor|status>");
+
 			getConfig().options().copyDefaults(true);
-	    }
-	    ConfigurationSerialization.registerClass(TI_Data.class, "TI_Data");
-	    townnames.addAll(tlist.keySet());
-	    Collections.sort(townnames);
-	    this.townnames = getTowns().getStringList("townnames");
-	    reloadTowns();
-	    reloadTlist();
+		}
+		ConfigurationSerialization.registerClass(TI_Data.class, "TI_Data");
+		townnames.addAll(tlist.keySet());
+		Collections.sort(townnames);
+		this.townnames = getTowns().getStringList("townnames");
+		reloadTowns();
+		reloadTlist();
 	}
+
 	@Override
-	public void onDisable(){
+	public void onDisable() {
 		getTowns().set("townnames", this.townnames);
-	    saveTowns();
+		saveTowns();
 		saveConfig();
 		saveTlist();
 	}
-	public void reloadTlist(){
+
+	public void reloadTlist() {
 		if (this.tlistFile == null) {
 			this.tlistFile = new File(getDataFolder(), "tlist.yml");
 		}
@@ -77,16 +83,18 @@ public class TI_Main extends JavaPlugin {
 			tlist.put(key, data);
 		}
 	}
+
 	public FileConfiguration getTlist() {
 		if (this.tlistf == null) {
 			reloadTlist();
 		}
 		return this.tlistf;
 	}
+
 	public void saveTlist() {
 		if ((this.tlistf == null) || (this.tlistFile == null))
-		return;
-		try{
+			return;
+		try {
 			for (Map.Entry<String, TI_Data> entry : tlist.entrySet()) {
 				tlistf.set(entry.getKey(), entry.getValue());
 			}
@@ -95,33 +103,39 @@ public class TI_Main extends JavaPlugin {
 			getLogger().log(Level.SEVERE, "Could not save config to " + this.tlistFile, ex);
 		}
 	}
-	public void reloadTowns(){
+
+	public void reloadTowns() {
 		if (this.townsFile == null) {
 			this.townsFile = new File(getDataFolder(), "towns.yml");
 		}
 		this.towns = YamlConfiguration.loadConfiguration(this.townsFile);
 	}
+
 	public FileConfiguration getTowns() {
 		if (this.towns == null) {
 			reloadTowns();
 		}
 		return this.towns;
 	}
+
 	public void saveTowns() {
 		if ((this.towns == null) || (this.townsFile == null))
-		return;
+			return;
 		try {
 			getTowns().save(this.townsFile);
 		} catch (IOException ex) {
 			getLogger().log(Level.SEVERE, "Could not save config to " + this.townsFile, ex);
 		}
 	}
+
 	public String color(String message) {
 		return ChatColor.translateAlternateColorCodes('&', message);
 	}
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		String tipermission = getConfig().getString("tipermission");
 		String tihelp = getConfig().getString("tihelp");
+		String tiadminhelp = getConfig().getString("tiadminhelp");
 		String tilistsize = getConfig().getString("tilistsize");
 		String tina = getConfig().getString("tina");
 		String wing1 = getConfig().getString("wing1");
@@ -129,38 +143,46 @@ public class TI_Main extends JavaPlugin {
 		String wing3 = getConfig().getString("wing3");
 		String wing4 = getConfig().getString("wing4");
 		String tiedit = getConfig().getString("tiedit");
+		String tiedithelpbasic = getConfig().getString("tiedithelpbasic");
+		String tiedithelpadmin = getConfig().getString("tiedithelpadmin");
 		tiedit = (color(tiedit));
 		int listnum;
-		if(cmd.getName().equalsIgnoreCase("ti")){
-			if (args.length == 0){
+		if (cmd.getName().equalsIgnoreCase("ti")) {
+			if (args.length == 0) {
 				sender.sendMessage(color(tihelp));
-			} else if (args.length == 1){
-				if (args[0].toLowerCase().equals("help")){
-					if (sender.hasPermission("ti.help")){
+				if (sender.hasPermission("ti.admin.help"))
+					sender.sendMessage(color(tiadminhelp));
+			} else if (args.length == 1) {
+				if (args[0].toLowerCase().equals("help")) {
+					if (sender.hasPermission("ti.help")) {
 						sender.sendMessage(color(tihelp));
+						if (sender.hasPermission("ti.admin.help"))
+							sender.sendMessage(color(tiadminhelp));
 					} else {
 						sender.sendMessage(color(tipermission));
 					}
 					return true;
-				} else if (args[0].toLowerCase().equals("list")){
-					if(sender.hasPermission("ti.list")){
+				} else if (args[0].toLowerCase().equals("list")) {
+					if (sender.hasPermission("ti.list")) {
 						try {
 							listnum = 1;
 							String listname = getConfig().getString("listname");
 							int townnamessize = townnames.size();
 							sender.sendMessage(color(listname));
-							int startfrom = (listnum*10)-10;
-							for (int i=startfrom;i<startfrom+10;i++) {
+							int startfrom = (listnum * 10) - 10;
+							for (int i = startfrom; i < startfrom + 10; i++) {
 								if (townnames.size() == 0) {
 									sender.sendMessage(color("&aNo towns available."));
 									break;
 								}
-								int sidenum = (i+1);
-								if (i>townnames.size()) break;
-								if (i == townnamessize) break;
+								int sidenum = (i + 1);
+								if (i > townnames.size())
+									break;
+								if (i == townnamessize)
+									break;
 								sender.sendMessage(color("&6" + sidenum + ": &2" + townnames.get(i)));
 							}
-						} catch(NumberFormatException e){
+						} catch (NumberFormatException e) {
 							sender.sendMessage(color("&4Argument must be a whole number!"));
 						}
 					} else {
@@ -168,11 +190,13 @@ public class TI_Main extends JavaPlugin {
 					}
 				} else {
 					sender.sendMessage(color(tihelp));
+					if (sender.hasPermission("ti.admin.help"))
+						sender.sendMessage(color(tiadminhelp));
 				}
-			} else if (args.length == 2){
+			} else if (args.length == 2) {
 				String townname = args[1];
-				if (args[0].toLowerCase().equals("add")){
-					if (sender.hasPermission("ti.admin.add")){
+				if (args[0].toLowerCase().equals("add")) {
+					if (sender.hasPermission("ti.admin.add")) {
 						if ((this.tlist.containsKey(townname)) || (this.townnames.contains(townname))) {
 							String townpresent = getConfig().getString("townpresent");
 							sender.sendMessage(color(townpresent));
@@ -182,15 +206,15 @@ public class TI_Main extends JavaPlugin {
 							tlist.put(townname.toLowerCase(), newtown);
 							String townadd = getConfig().getString("townadd");
 							sender.sendMessage(color(townadd));
-						    Collections.sort(townnames);
-						    reloadTlist();
+							Collections.sort(townnames);
+							reloadTlist();
 						}
 					} else {
 						sender.sendMessage(color(tipermission));
 					}
 					return true;
-				} else if (args[0].toLowerCase().equals("remove")){
-					if (sender.hasPermission("ti.admin.remove")){
+				} else if (args[0].toLowerCase().equals("remove")) {
+					if (sender.hasPermission("ti.admin.remove")) {
 						if (this.tlist.containsKey(townname)) {
 							this.tlist.remove(townname);
 							sender.sendMessage(color("&aTown data deleted."));
@@ -203,30 +227,32 @@ public class TI_Main extends JavaPlugin {
 						sender.sendMessage(color(tipermission));
 					}
 					return true;
-				} else if (args[0].toLowerCase().equals("list")){
-					if(sender.hasPermission("ti.list")){
+				} else if (args[0].toLowerCase().equals("list")) {
+					if (sender.hasPermission("ti.list")) {
 						try {
 							listnum = Integer.parseInt(args[1]);
-							int startfrom = (listnum*10)-10;
-							if (startfrom>townnames.size()){
+							int startfrom = (listnum * 10) - 10;
+							if (startfrom > townnames.size()) {
 								sender.sendMessage(color(tilistsize));
 							} else {
 								String listname = getConfig().getString("listname");
 								int townnamessize = townnames.size();
 								sender.sendMessage(color(listname));
-								for (int i=startfrom;i<startfrom+10;i++) {
-									int sidenum = (i+1);
-									if (i>townnames.size()) break;
+								for (int i = startfrom; i < startfrom + 10; i++) {
+									int sidenum = (i + 1);
+									if (i > townnames.size())
+										break;
 									sender.sendMessage(color("&6" + sidenum + ": &2" + townnames.get(i)));
-									if (i == townnamessize) break;
+									if (i == townnamessize)
+										break;
 								}
 							}
-						} catch(NumberFormatException e){
+						} catch (NumberFormatException e) {
 							sender.sendMessage(color("&4Argument must be a whole number!"));
 						}
 					} else {
 						sender.sendMessage(color(tipermission));
-					} 
+					}
 				} else if (args[0].toLowerCase().equals("info")) {
 					if (sender.hasPermission("ti.info")) {
 						if (args.length == 2) {
@@ -236,7 +262,7 @@ public class TI_Main extends JavaPlugin {
 								return true;
 							} else {
 								Vector location = data.getLocation();
-								sender.sendMessage(color("&6 " + data.getTName()));
+								sender.sendMessage(color("&a " + data.getTName()));
 								sender.sendMessage(color("&6- " + "Status: &a" + data.getTStatus()));
 								sender.sendMessage(color("&6- " + "Mayor: &a" + data.getMayor()));
 								sender.sendMessage(color("&6- " + "Vice Mayor: &a" + data.getVMayor()));
@@ -246,6 +272,8 @@ public class TI_Main extends JavaPlugin {
 								else
 									sender.sendMessage(color("&6- " + "Location: &anull"));
 								sender.sendMessage(color("&6- " + "Plots Available: &a" + data.getPAvail()));
+								sender.sendMessage(color("&6- " + "MOTD: &a" + data.getMOTD()));
+								sender.sendMessage(color("&6- " + "Rating: &a" + data.getRating() + "%"));
 							}
 						}
 					} else {
@@ -254,14 +282,14 @@ public class TI_Main extends JavaPlugin {
 				}
 			} else if (args[0].toLowerCase().equals("edit")) {
 				TI_Data data = tlist.get(args[1].toLowerCase());
+				if (data == null) {
+					sender.sendMessage(color(tina));
+					return true;
+				}
 				if (args[2].toLowerCase().equals("mayor")) {
-					if (args.length == 4){
+					if (args.length == 4) {
 						if (!(sender.hasPermission("ti.admin.edit"))) {
 							sender.sendMessage(color(tipermission));
-							return true;
-						}
-						if (data == null) {
-							sender.sendMessage(color(tina));
 							return true;
 						}
 						sender.sendMessage(tiedit);
@@ -271,13 +299,9 @@ public class TI_Main extends JavaPlugin {
 						return true;
 					}
 				} else if (args[2].toLowerCase().equals("vmayor")) {
-					if (args.length == 4){
-						if (!((sender.hasPermission("ti.edit.vmayor") && (sender.getName() == data.getMayor())) || sender.hasPermission("ti.admin.edit"))) {
+					if (args.length == 4) {
+						if (!((sender.hasPermission("ti.basic.edit.vmayor") && (sender.getName() == data.getMayor())) || sender.hasPermission("ti.admin.edit"))) {
 							sender.sendMessage(color(tipermission));
-							return true;
-						}
-						if (data == null) {
-							sender.sendMessage(color(tina));
 							return true;
 						}
 						sender.sendMessage(tiedit);
@@ -288,12 +312,8 @@ public class TI_Main extends JavaPlugin {
 					}
 				} else if (args[2].toLowerCase().equals("wing")) {
 					if (args.length == 4) {
-						if (!((sender.hasPermission("ti.edit.wing") && (sender.getName() == data.getMayor())) || sender.hasPermission("ti.admin.edit"))) {
+						if (!((sender.hasPermission("ti.basic.edit.wing") && (sender.getName() == data.getMayor())) || sender.hasPermission("ti.admin.edit"))) {
 							sender.sendMessage(color(tipermission));
-							return true;
-						}
-						if (data == null) {
-							sender.sendMessage(color(tina));
 							return true;
 						}
 						if ((args[3].equalsIgnoreCase(wing1)) || (args[3].equalsIgnoreCase(wing2)) || (args[3].equalsIgnoreCase(wing2)) || (args[3].equalsIgnoreCase(wing4))) {
@@ -309,12 +329,8 @@ public class TI_Main extends JavaPlugin {
 					}
 				} else if (args[2].toLowerCase().equals("location")) {
 					if (args.length == 3) {
-						if(!((sender.hasPermission("ti.edit.location") && (sender.getName() == data.getMayor())) || sender.hasPermission("ti.admin.edit"))) {
+						if (!((sender.hasPermission("ti.basic.edit.location") && (sender.getName() == data.getMayor())) || sender.hasPermission("ti.admin.edit"))) {
 							sender.sendMessage(color(tipermission));
-							return true;
-						}
-						if (data == null) {
-							sender.sendMessage(color(tina));
 							return true;
 						}
 						if (!(sender instanceof Player)) {
@@ -331,12 +347,8 @@ public class TI_Main extends JavaPlugin {
 					}
 				} else if (args[2].toLowerCase().equals("plots")) {
 					if (args.length == 4) {
-						if(!((sender.hasPermission("ti.edit.plots") && (sender.getName() == data.getMayor())) || sender.hasPermission("ti.admin.edit"))) {
+						if (!((sender.hasPermission("ti.basic.edit.plots") && (sender.getName() == data.getMayor())) || sender.hasPermission("ti.admin.edit"))) {
 							sender.sendMessage(color(tipermission));
-							return true;
-						}
-						if (data == null) {
-							sender.sendMessage(color(tina));
 							return true;
 						}
 						if ((args[3].equalsIgnoreCase("yes")) || (args[3].equalsIgnoreCase("no")) || (args[3].equalsIgnoreCase("possibly"))) {
@@ -351,12 +363,8 @@ public class TI_Main extends JavaPlugin {
 					}
 				} else if (args[2].toLowerCase().equals("status")) {
 					if (args.length == 4) {
-						if(!(sender.hasPermission("ti.admin.edit"))) {
+						if (!(sender.hasPermission("ti.admin.edit"))) {
 							sender.sendMessage(color(tipermission));
-							return true;
-						}
-						if (data == null) {
-							sender.sendMessage(color(tina));
 							return true;
 						}
 						if ((args[3].equalsIgnoreCase("Village")) || (args[3].equalsIgnoreCase("Town")) || (args[3].equalsIgnoreCase("City"))) {
@@ -369,17 +377,49 @@ public class TI_Main extends JavaPlugin {
 						sender.sendMessage(color("&cUse as /ti edit <town> status <status>"));
 						return true;
 					}
+				} else if (args[2].toLowerCase().equals("motd")) {
+					if (!((sender.hasPermission("ti.basic.edit.motd") && (sender.getName() == data.getMayor())) || sender.hasPermission("ti.admin.edit"))) {
+						sender.sendMessage(color(tipermission));
+						return true;
+					}
+					StringBuilder buffer = new StringBuilder();
+					for (int i = 3; i < args.length; i++) {
+						buffer.append(' ').append(args[i]);
+					}
+					String motd = buffer.toString();
+					data.setMOTD(motd);
+					sender.sendMessage(tiedit);
+				} else if (args[2].equalsIgnoreCase("rating")) {
+					if (!sender.hasPermission("ti.admin.edit"))
+						return true;
+					try {
+						Double.parseDouble(args[3]);
+					} catch (NumberFormatException e) {
+						sender.sendMessage(color("&cRating must be a number!"));
+						return true;
+					}
+					if ((Double.parseDouble(args[3]) > 100)) {
+						sender.sendMessage(color("&cNumber must be below 100!"));
+						return true;
+					}
+					sender.sendMessage(tiedit);
+					data.setRating(Double.parseDouble(args[3]));
 				} else {
 					if (!(sender.hasPermission("ti.admin.edit"))) {
-						sender.sendMessage(color("&cUse as /ti edit <town> [mayor, vmayor, location, plots, status]"));
+						sender.sendMessage(color(tiedithelpbasic));
 					} else {
-						sender.sendMessage(color("&cUse as /ti edit <town> [vmayor, location, plots]"));
+						sender.sendMessage(color(tiedithelpbasic));
+						sender.sendMessage(color(tiedithelpadmin));
 					}
 				}
+			} else {
+				sender.sendMessage(color(tihelp));
+				if (sender.hasPermission("ti.admin.help"))
+					sender.sendMessage(color(tiadminhelp));
 			}
 		} else {
 			sender.sendMessage(color(tina));
 		}
-	return true;
+		return true;
 	}
 }
